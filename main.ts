22049c264a833787c0d1c32ebc37a447e79f3923
angelinaaaaazhu-lab <angelinaaaaazhu@gmail.com>
2025-10-17 10:36:39 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-let mySprite: Sprite = null
 scene.setBackgroundImage(assets.image`BackgroundmyImage`)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 5 5 . . . . . . . . 
+    . . . . 5 5 5 5 5 5 5 . . . . . 
+    . . . 5 5 5 5 5 5 5 5 5 . . . . 
+    . . 5 5 5 5 5 5 5 5 5 5 . . . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+    . . . 5 5 5 5 5 5 5 5 5 . . . . 
+    . . . . 5 5 5 5 5 5 5 . . . . . 
+    . . . . . 5 5 5 . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite)
